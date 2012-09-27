4a50e50052bb29714ce9a000d63ee725a4a3012f
Tony Lee <github@roganartu.com>
2012-09-27 16:33:34 +1000

    Added barebones echat test program

---
+++ test/echat.c
@@ -0,0 +1,22 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  echat.c
+ *
+ *    Description:  echat crypto test program
+ *
+ *        Version:  0.0.1
+ *        Created:  27/09/12 16:12:49
+ *
+ *         Author:  Tony Lee (Roganartu), uni@roganartu.com
+ *   Organisation:  UQ Bachelor of Engineering
+ *
+ *          Notes:  
+ * =============================================================================
+ */
+#include "echat.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ test/echat.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================
+ *
+ *       Filename:  echat.h
+ *
+ *    Description:  echat crypto test program
+ *
+ *        Version:  0.0.1
+ *        Created:  27/09/12 16:13:43
+ *
+ *         Author:  Tony Lee (Roganartu), uni@roganartu.com
+ *   Organisation:  UQ Bachelor of Engineering
+ *
+ *          Notes:  
+ * =====================================================================
+ */
+#ifndef COMP3301_ECHAT_H
+#define COMP3301_ECHAT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define CRYPTO_DEVICE "/dev/crypto"
+
+#endif
